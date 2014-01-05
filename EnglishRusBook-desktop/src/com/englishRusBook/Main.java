fb360ea97171ede934cc87df18dc0f32ef55dbab
Golars <titorens@gmail.com>
2014-01-05 11:53:31 +0200

    +start

---
+++ EnglishRusBook-desktop/src/com/englishRusBook/Main.java
@@ -0,0 +1,16 @@
+package com.englishRusBook;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "EnglishRusBook";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new EnglishRusBook(), cfg);
+	}
+}
